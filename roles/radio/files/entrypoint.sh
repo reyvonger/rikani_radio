7c09f6766500a93e41be97a993ee758dd60bf1ee
vladimir <vladimir@pankin.org>
2021-01-26 13:43:27 +0300

    change vars

--- roles/radio/files/entrypoint.sh
+++ roles/radio/files/entrypoint.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
 
-VBR="3000k"
-FPS="12"
-QUAL="superfast"
 YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"
-VIDEO_SOURCE="/video/play.mp4"
 
 ffmpeg \
     -re -f lavfi -i "movie=filename=$VIDEO_SOURCE:loop=0, setpts=N/(FRAME_RATE*TB)" \
